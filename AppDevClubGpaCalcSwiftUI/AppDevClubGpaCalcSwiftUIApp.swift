da2fc8f0d9caa7481a5b5467b83e03d423971f1e
Kevin Barnes <kevin@donniebarnes.com>
2022-10-17 19:30:09 -0400

    Initial Commit

---
+++ AppDevClubGpaCalcSwiftUI/AppDevClubGpaCalcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevClubGpaCalcSwiftUIApp.swift
+//  AppDevClubGpaCalcSwiftUI
+//
+//  Created by Kevin Barnes on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AppDevClubGpaCalcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
